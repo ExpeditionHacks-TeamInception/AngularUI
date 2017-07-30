ea6e150987898226cdd4d0619b5f136cbb37fca9
prakriti_khanal <pkhanal@us.ibm.com>
2017-07-30 11:42:00 -0500

    finalized

--- mobility/src/app/home/home.component.ts
+++ mobility/src/app/home/home.component.ts
@@ -46,9 +46,41 @@ export class HomeComponent implements OnInit {
         map.addObject(circle);
         map.setCenter({lat: lacLan, lng: locLon});
         map.setZoom(14);
+        addInfoBubble(lacLan,locLon)
       });
       // setTimeout(this.moveMapToLocation(this.map, locLan, locLon), 500000);
     }
+
+    function addInfoBubble(lacLan,locLon) {
+      var group = new H.map.Group();
+
+      map.addObject(group);
+
+      // add 'tap' event listener, that opens info bubble, to the group
+      group.addEventListener('pointermove', function (evt) {
+        // event target is the marker itself, group is a parent event target
+        // for all objects that it contains
+        var bubble =  new H.ui.InfoBubble(evt.target.getPosition(), {
+          // read custom data
+          content: evt.target.getData()
+        });
+        // show info bubble
+        ui.addBubble(bubble);
+      }, false);
+
+      addMarkerToGroup(group, {lat:lacLan, lng:locLon},
+        '<div>You are here</div>');
+
+    }
+
+    function addMarkerToGroup(group, coordinate, html) {
+      var marker = new H.map.Marker(coordinate);
+      // add custom data to the marker
+      marker.setData(html);
+      group.addObject(marker);
+    }
+
+
 
 
     // Now use the map as required...
