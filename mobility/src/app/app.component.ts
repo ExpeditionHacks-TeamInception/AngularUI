10a2b27af2ca19dd85b853a7dc4a723db8500555
prakriti_khanal <pkhanal@us.ibm.com>
2017-07-29 20:18:21 -0500

    added current location

--- mobility/src/app/app.component.ts
+++ mobility/src/app/app.component.ts
@@ -1,60 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
-declare var H:any;
+declare var H: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'app';
   platform: any;
-  map: any;
 
   ngOnInit() {
-   
+
+    this.platform = new H.service.Platform({
+      app_id: 'DemoAppId01082013GAL',
+      app_code: 'AJKnXv84fjrb0KIHawS0Tg',
+      useCIT: true,
+      useHTTPS: true
+    });
+    let defaultLayers = this.platform.createDefaultLayers();
+    let map = new H.Map(document.getElementById('map'),
+      defaultLayers.normal.map);
+    let behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+    let ui = H.ui.UI.createDefault(map, defaultLayers);
+
+    if (navigator.geolocation) {
+      let locLan: any;
+      let locLon: any;
+      navigator.geolocation.getCurrentPosition(function (location) {
+        locLan = location.coords.latitude;
+        locLon = location.coords.longitude;
+        map.setCenter({lat: locLan, lng: locLon});
+        map.setZoom(14);
+      });
+      // debugger;
+      // setTimeout(this.moveMapToLocation(this.map, locLan, locLon), 500000);
+    }
 
 
-
-
-
-  /**
-   * Boilerplate map initialization code starts below:
-   */
-
-//Step 1: initialize communication with the platform
-  this.platform = new H.service.Platform({
-    app_id: 'DemoAppId01082013GAL',
-    app_code: 'AJKnXv84fjrb0KIHawS0Tg',
-    useCIT: true,
-    useHTTPS: true
-  });
-  var defaultLayers = this.platform.createDefaultLayers();
-
-  //Step 2: initialize a map  - not specificing a location will give a whole world view.
-  this.map = new H.Map(document.getElementById('map'),
-    defaultLayers.normal.map);
-
-  //Step 3: make the map interactive
-  // MapEvents enables the event system
-  // Behavior implements default interactions for pan/zoom (also on mobile touch environments)
-  var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(this.map));
-
-  // Create the default UI components
-  var ui = H.ui.UI.createDefault(this.map, defaultLayers);
-
-  // Now use the map as required...
-  this.moveMapToBerlin(this.map);
-  }
-
-   /**
-   * Moves the map to display over Berlin
-   *
-   * @param  {H.Map} map      A HERE Map instance within the application
-   */
-  moveMapToBerlin(map){
-    map.setCenter({lat:52.5159, lng:13.3777});
-    map.setZoom(14);
+    // Now use the map as required...
   }
 }
